a0bce9933ea13c58e9bd7d30e75996c8177caf9f
saviodev21 <saviosantossilva19@gmail.com>
2023-02-25 17:04:56 -0300

    impelemntando loading

--- assets/script.js
+++ assets/script.js
@@ -23,40 +23,37 @@ const projects = [
 	  description: "Descrição do Projeto 3",
 	  image: "https://via.placeholder.com/150x150"
 	}
-  ];
+];
   
-  const projectTableBody = document.querySelector("#projectTableBody");
-  
-  projects.forEach(project => {
-	// Cria uma nova linha
-	const tr = document.createElement("tr");
-	
-    // Cria uma célula com o nome do projeto
-	const tdName = document.createElement("td");
-	tdName.textContent = project.name;
-	
-    // Cria uma célula com a descrição do projeto
-	const tdDescription = document.createElement("td");
-	tdDescription.textContent = project.description;
-  
-	
-	// Cria uma célula com a imagem do projeto
-	const tdImage = document.createElement("td");
-	const img = document.createElement("img");
-	img.src = project.image;
-	img.alt = project.name;
-	img.width = 150;
-	img.height = 150;
-	tdImage.appendChild(img);
-  
-	tr.appendChild(tdName);
-	tr.appendChild(tdDescription);
-	tr.appendChild(tdImage);
-  
-	
-	// Adiciona a linha na tabela
-	projectTableBody.appendChild(tr);
-  });
-  
+const projectTableBody = document.querySelector("#projectTableBody");
+
+projects.forEach(project => {
+// Cria uma nova linha
+const tr = document.createElement("tr");
+
+// Cria uma célula com o nome do projeto
+const tdName = document.createElement("td");
+tdName.textContent = project.name;
+
+// Cria uma célula com a descrição do projeto
+const tdDescription = document.createElement("td");
+tdDescription.textContent = project.description;
 
 
+// Cria uma célula com a imagem do projeto
+const tdImage = document.createElement("td");
+const img = document.createElement("img");
+img.src = project.image;
+img.alt = project.name;
+img.width = 150;
+img.height = 150;
+tdImage.appendChild(img);
+
+tr.appendChild(tdName);
+tr.appendChild(tdDescription);
+tr.appendChild(tdImage);
+
+
+// Adiciona a linha na tabela
+projectTableBody.appendChild(tr);
+});
